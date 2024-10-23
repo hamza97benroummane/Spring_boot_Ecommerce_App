c079ecbc0783ad2513774695fb66b039a5f74b82
hamza97benroummane <hamza.benroummane@gmail.com>
2024-10-23 16:58:48 +0100

    JpaRepo

---
+++ src/main/java/com/Ecom_Hamza/ecommerce/entity/dao/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.Ecom_Hamza.ecommerce.entity.dao;
+
+import com.Ecom_Hamza.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory" , path = "product-category")
+public interface ProductCategoryRepository  extends JpaRepository<ProductCategory, Long> {
+}
---
+++ src/main/java/com/Ecom_Hamza/ecommerce/entity/dao/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.Ecom_Hamza.ecommerce.entity.dao;
+
+import com.Ecom_Hamza.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
