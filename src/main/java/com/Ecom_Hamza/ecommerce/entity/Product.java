06faee01e93d9fd8e5f318d6c4224be4d6804f5e
hamza97benroummane <hamza.benroummane@gmail.com>
2024-10-23 16:01:27 +0100

    Develop JPA Entities

---
+++ src/main/java/com/Ecom_Hamza/ecommerce/entity/Product.java
@@ -0,0 +1,55 @@
+package com.Ecom_Hamza.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private  ProductCategory category;
+
+
+    @Column(name = "sku")
+    private String Sku;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "units_in_stock")
+    private int unitsInStocke;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @Column(name ="last_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+
+}
