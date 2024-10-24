c51f751786d63a7066240fe532b885b7b6d0beaa
hamza97benroummane <hamza.benroummane@gmail.com>
2024-10-24 16:58:28 +0100

    add CrossOrigine

--- src/main/java/com/Ecom_Hamza/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/Ecom_Hamza/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package com.Ecom_Hamza.ecommerce.dao;
 
 import com.Ecom_Hamza.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
